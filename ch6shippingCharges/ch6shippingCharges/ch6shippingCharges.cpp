d1b20bfec3d5fb311484e8e6f0b02025693dc4ed
Kevin Bell <bellKevin@pm.me>
2022-10-07 10:13:08 -0600

    Update ch6shippingCharges.cpp

--- ch6shippingCharges/ch6shippingCharges/ch6shippingCharges.cpp
+++ ch6shippingCharges/ch6shippingCharges/ch6shippingCharges.cpp
@@ -4,11 +4,11 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
+double weight, shippingCost;
+int blocks, remainderMiles, miles;
+double calculateCharge(double, int);
 int main()
 {
-    double weight, shippingCost;
-    int blocks, remainderMiles, miles;
     cout << "Ch 6 Shipping Charges by Kevin Bell\n\n";
     cout << "   Fast Freight Shipping Company\n";
     cout << "Package Shipping Charges Calculator\n\n";
@@ -19,24 +19,7 @@ int main()
         cout << "Enter shipping distance in miles: ";
         cin >> miles;
         cout << endl;
-        //shippingCost = calculateCharge(weight, miles);
-        blocks = miles / 500;
-        remainderMiles = miles % 500;
-        if (remainderMiles >= 1) {
-            blocks = blocks + 1;
-        }
-        if (weight <= 2) {
-            shippingCost = 3.10 * blocks;
-        }
-        else if (weight > 2 && weight <= 6) {
-            shippingCost = 4.20 * blocks;
-        }
-        else if (weight > 6 && weight <= 10) {
-            shippingCost = 5.30 * blocks;
-        }
-        else if (weight > 10) {
-            shippingCost= 6.40 * blocks;
-        }
+        shippingCost = calculateCharge(weight, miles);
         cout << "Shipping cost: $" << shippingCost << endl << endl;
         cout << "Enter package weight in lbs (or 0 to exit): ";
         cin >> weight;
@@ -45,22 +28,22 @@ int main()
     return 0;
 }
 
-double calculateCharge(double weight, double miles) {
-    double x;
-    if (weight <=2) {
-       x = ((miles/500) * 3.10);
-       return x;
+double calculateCharge(double weight, int miles) {
+    blocks = miles / 500;
+    remainderMiles = miles % 500;
+    if (remainderMiles >= 1) {
+        blocks = blocks + 1;
+    }
+    if (weight <= 2) {
+        return shippingCost = 3.10 * blocks;
     }
     else if (weight > 2 && weight <= 6) {
-       x = ((miles / 500) * 4.20);
-       return x;
+        return shippingCost = 4.20 * blocks;
     }
-    else if (weight > 6 && weight <=10) {
-        x = ((miles / 500) * 5.30);
-        return x;
+    else if (weight > 6 && weight <= 10) {
+        return shippingCost = 5.30 * blocks;
     }
     else if (weight > 10) {
-        x = ((miles / 500) * 6.40);
-        return x;
+        return shippingCost = 6.40 * blocks;
     }
 }
