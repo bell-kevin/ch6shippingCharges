4008929c7a314cd3a61d63eb9b47ed16bad3a1a3
Kevin Bell <bellKevin@pm.me>
2022-10-07 09:25:57 -0600

    Update ch6shippingCharges.cpp

--- ch6shippingCharges/ch6shippingCharges/ch6shippingCharges.cpp
+++ ch6shippingCharges/ch6shippingCharges/ch6shippingCharges.cpp
@@ -2,7 +2,7 @@
 //
 
 #include <iostream>
-#include "ch6shippingCharges.h"
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -13,11 +13,27 @@ int main()
     cout << "Package Shipping Charges Calculator\n\n";
     cout << "Enter package weight in lbs (or 0 to exit): ";
     cin >> weight;
+    cout << setprecision(2) << fixed;
     while (weight !=0) {
         cout << "Enter shipping distance in miles: ";
         cin >> miles;
         cout << endl;
-        calculateCharge();
+       //shippingCost = calculateCharge(weight, miles);
+        if (weight <= 2 && miles <= 500) {
+            shippingCost = 3.10;
+        }
+        else if (weight <= 2 && miles > 500 && miles <= 1000) {
+            shippingCost = 3.10*2;
+        }
+        else if (weight > 2 && weight <= 6 && miles <=500) {
+            shippingCost = 4.20;
+        }
+        else if (weight > 6 && weight <= 10 && miles <= 500) {
+            shippingCost = 5.30;
+        }
+        else if (weight > 10 && miles <= 500) {
+            shippingCost= 6.40;
+        }
         cout << "Shipping cost: $" << shippingCost << endl << endl;
         cout << "Enter package weight in lbs (or 0 to exit): ";
         cin >> weight;
@@ -27,5 +43,21 @@ int main()
 }
 
 double calculateCharge(double weight, double miles) {
-    //code here
+    double x;
+    if (weight <=2) {
+       x = ((miles/500) * 3.10);
+       return x;
+    }
+    else if (weight > 2 && weight <= 6) {
+       x = ((miles / 500) * 4.20);
+       return x;
+    }
+    else if (weight > 6 && weight <=10) {
+        x = ((miles / 500) * 5.30);
+        return x;
+    }
+    else if (weight > 10) {
+        x = ((miles / 500) * 6.40);
+        return x;
+    }
 }
