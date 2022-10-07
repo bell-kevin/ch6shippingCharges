bf21527f165e5efc9d37757a67b982f9e0e9ff1c
Kevin Bell <bellKevin@pm.me>
2022-10-07 12:02:46 -0600

    Update ch6shippingCharges.cpp

--- ch6shippingCharges/ch6shippingCharges/ch6shippingCharges.cpp
+++ ch6shippingCharges/ch6shippingCharges/ch6shippingCharges.cpp
@@ -24,26 +24,31 @@ int main()
         cout << "Enter package weight in lbs (or 0 to exit): ";
         cin >> weight;
     }
+    //Keep window open til done
     system("pause");
     return 0;
 }
 
 double calculateCharge(double weight, int miles) {
+    const double WEIGHT_TWO =3.10;
+    const double WEIGHT_SIX = 4.20;
+    const double WEIGHT_TEN = 5.30;
+    const double WEIGHT_TEN_PLUS = 6.40;
     blocks = miles / 500;
     remainderMiles = miles % 500;
     if (remainderMiles >= 1) {
         blocks = blocks + 1;
     }
     if (weight <= 2) {
-        return shippingCost = 3.10 * blocks;
+        return WEIGHT_TWO * blocks;
     }
     else if (weight > 2 && weight <= 6) {
-        return shippingCost = 4.20 * blocks;
+        return WEIGHT_SIX * blocks;
     }
     else if (weight > 6 && weight <= 10) {
-        return shippingCost = 5.30 * blocks;
+        return WEIGHT_TEN * blocks;
     }
     else if (weight > 10) {
-        return shippingCost = 6.40 * blocks;
+        return WEIGHT_TEN_PLUS * blocks;
     }
 }
